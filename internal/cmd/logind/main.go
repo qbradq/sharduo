64f2b53ab191e90a03e5427a75cdf2904768dbb6
Norman B. Lancaster <qbradq@gmail.com>
2020-08-19 18:43:20 -0500

    Re-wrote logind

--- internal/cmd/logind/main.go
+++ internal/cmd/logind/main.go
@@ -1,12 +1,12 @@
 package logind
 
 import (
-	"errors"
 	"log"
 	"net"
+	"time"
 
-	"github.com/qbradq/sharduo/internal/network"
-	"github.com/qbradq/sharduo/lib/uo"
+	"github.com/qbradq/sharduo/lib/clientpacket"
+	"github.com/qbradq/sharduo/lib/serverpacket"
 )
 
 // Main is the entry point for logind.
@@ -28,6 +28,73 @@ func Main() {
 	}
 }
 
+func handleConnection(c *net.TCPConn) {
+	defer c.Close()
+	c.SetKeepAlive(false)
+	c.SetLinger(0)
+	c.SetNoDelay(true)
+	c.SetReadBuffer(64 * 1024)
+	c.SetWriteBuffer(128 * 1024)
+	c.SetDeadline(time.Now().Add(time.Minute * 15))
+	r := clientpacket.NewReader(c)
+
+	// Connection header
+	if err := r.ReadConnectionHeader(); err != nil {
+		log.Println("Client disconnected during header due to", err)
+		return
+	}
+
+	// Account login packet
+	cp, err := r.ReadPacket()
+	if err != nil {
+		log.Println("Client disconnected waiting for account login", err)
+		return
+	}
+	alp, ok := cp.(*clientpacket.AccountLogin)
+	if !ok {
+		log.Println("Client sent wrong packet waiting for account login")
+		return
+	}
+	log.Println("User login", alp.Username, alp.Password)
+
+	// Server list packet
+	var sp serverpacket.Packet
+	sp = &serverpacket.ServerList{
+		Entries: []serverpacket.ServerListEntry{
+			{
+				Name: "Trammie Time LOCAL DEV",
+				IP:   net.ParseIP("127.0.0.1"),
+			},
+		},
+	}
+	sp.Write(c)
+
+	// Select server packet
+	cp, err = r.ReadPacket()
+	if err != nil {
+		log.Println("Client disconnected waiting for select server", err)
+		return
+	}
+	ssp, ok := cp.(*clientpacket.SelectServer)
+	if !ok {
+		log.Println("Client sent wrong packet waiting for select server")
+		return
+	}
+	log.Println("Selected server", ssp.Index)
+
+	// Connect to game server packet
+	sp = &serverpacket.ConnectToGameServer{
+		IP:   net.ParseIP("127.0.0.1"),
+		Port: 7777,
+		Key:  []byte{0xBA, 0xAD, 0xF0, 0x0D},
+	}
+	sp.Write(c)
+
+	// End of login session
+	return
+}
+
+/*
 func handleConnection(c *net.TCPConn) {
 	var ok bool
 
@@ -76,3 +143,4 @@ func handleConnection(c *net.TCPConn) {
 	}, 7777)
 	cc.SendPacket(cgsp)
 }
+*/
