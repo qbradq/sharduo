e01c2d54c4963e1ccbfe192de1c7d7569f881de0
Norman B. Lancaster <qbradq@gmail.com>
2022-12-14 22:54:13 +0000

    Starting on file formats

---
+++ lib/uo/file/index.go
@@ -0,0 +1,10 @@
+package file
+
+// IndexEntry represents one entry in an index file.
+type IndexEntry struct {
+}
+
+// Index represents a MUL file index. These files typically have the same name
+// as the MUL file they apply to with the .idx extension.
+type Index struct {
+}
