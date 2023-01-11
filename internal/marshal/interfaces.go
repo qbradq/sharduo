0a8d21956c9a2c9d810517f82ead5ff807d0c9a5
Norman B. Lancaster <qbradq@gmail.com>
2023-01-11 02:41:09 -0600

    Missed s save

--- internal/marshal/interfaces.go
+++ internal/marshal/interfaces.go
@@ -3,5 +3,11 @@ package marshal
 // Marshaler is an object that knows how to marshal itself into a binary format.
 type Marshaler interface {
 	// Marshal writes the binary representation of the object to w.
-	Marshal()
+	Marshal(w *Writer)
+}
+
+// Unmarshaler is an object that knows how to unmarshal itself from binary.
+type Unmarshaler interface {
+	// Unmarshal reads the binary representation of the object from r.
+	Unmarshal(r *Reader)
 }
