657dc35531a58105d4d10ee04aa7cd42784d2ae3
Norman B. Lancaster <qbradq@gmail.com>
2023-01-11 02:40:49 -0600

    Started on marshal package

---
+++ internal/marshal/interfaces.go
@@ -0,0 +1,7 @@
+package marshal
+
+// Marshaler is an object that knows how to marshal itself into a binary format.
+type Marshaler interface {
+	// Marshal writes the binary representation of the object to w.
+	Marshal()
+}
